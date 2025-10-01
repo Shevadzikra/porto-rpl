539c8451bed322eacace912d26735a9cf5e94741
Lavianzy <shevaihsan5@gmail.com>
2025-10-01 19:43:07 +0700

    add meta tag google search console

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'SMKN 3 JEMBER - Rekayasa Perangkat Lunak',
   description: 'Website Resmi Jurusan Rekayasa Perangkat Lunak SMKN 3 Jember - Home of Future Software Engineers',
+    other: {
+    "google-site-verification": "5aIAW2c6mcX-W8JPqSUQ_IGte85gz8KWwQ3HBVAabEE",
+  }
 }
 
 export default function RootLayout({ children }) {
