4599aca6939c20197224db50f0270b2ff6f00d10
Lavianzy <shevaihsan5@gmail.com>
2025-10-01 19:39:37 +0700

    add meta tag google search console

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
+      <head>
+        <meta name="google-site-verification" content="5aIAW2c6mcX-W8JPqSUQ_IGte85gz8KWwQ3HBVAabEE" />
+      </head>
       <body className="min-h-screen bg-white">
         {children}
       </body>
